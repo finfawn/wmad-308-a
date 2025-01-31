6215e7f29e554d72bd07eb422271d2a72178bb71
robertpaciteng <robertjrpacitengp@gmail.com>
2025-01-31 16:46:54 +0800

    Test

---
+++ flutter_00_test_run_paciteng_robert/android/app/src/main/kotlin/com/example/flutter_00_test_run_paciteng_robert/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_00_test_run_paciteng_robert
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- flutter_00_test_run_polled_kenneth/android/app/src/main/kotlin/com/example/flutter_00_test_run_polled_kenneth/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_00_test_run_polled_kenneth
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
